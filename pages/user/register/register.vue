07a8f46d7227828c53dca93dee6023eff244d252
zhangyang <690196784@qq.com>
2020-12-20 21:41:03 +0800

    移动商户端第一次提交

---
+++ pages/user/register/register.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="mingxi">
+		<cu-custom bgColor="bg-gradual-blue" :isBack="true"><block slot="content">选择注册方式</block></cu-custom>
+		<view class="ticheng">
+			<block v-for="(item,index) of list" :key="index">
+				<li class="tc" @click="jump(item.id)">
+					<view class="tc_l">
+						<span>在线预约-{{item.card.bank_num.substr(item.card.bank_num.length-4)}}</span><br/>
+						<span class="smallText">预约客户经理上门服务，一站搞定POS、条码等多样化 收款需求！{{item.create_time}}</span>
+					</view>
+					 <view class="tc_2">
+						 <img src="@/imgs/5.png" style="float: right;margin-right: 30%;">
+					</view> 
+				</li>
+			</block>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniIcon from "@/components/uni/uni-icon/uni-icon.vue"
+	export default {
+		data() {
+			return {
+				list:[],
+				num:0,
+			};
+		},
+		onLoad() {  
+			this._load()
+		},
+		components: {uniIcon},
+		methods:{
+			_load(){ 
+				this.list=this.$api.json.get_tx_log
+			},
+			jump_cash(){
+				uni.navigateTo({
+					url:'/pages/user/fenxiao/tixian/tixian'
+				})
+			},
+			jump(id){
+				//申请提现成功页面
+				// uni.navigateTo({
+				// 	url:'/pages/user/fenxiao/success/success?id='+id
+				// })
+				uni.navigateTo({
+					url:'/pages/user/register/register1'
+				})
+			},
+			change(e){
+				this.num=e 
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+.mingxi{
+	.head{display: flex;margin: 10px 0;margin-top: 45px;}
+	.head_l{display: flex;width: 85%;justify-content: space-around;}
+	.head_r{width: 15%;text-align: center;}
+	.head_l_1{border: 1px solid #F2F2F2;padding: 0px 15px;line-height: 25px;}
+	.head_btn{margin:10px 20px 0;border: 1px solid #F2F2F2;padding: 0px 15px;line-height: 25px;}
+	.ling{color: #E1461D;border: 1px solid #E1461D;padding: 0px 15px;line-height: 25px;}
+	.shouyi{border-top: 1px solid #EBEBEB;border-bottom:1px solid #EBEBEB; background-color: #FAFAFA;display: flex;height: 30px;
+	line-height: 30px;padding: 3px 10px;margin-top: 15px;}
+	.sy_l{width: 50%;}
+	.sy_l span{font-weight: bold;}
+	.ticheng li:nth-of-type(odd){ background-color: #EEEEEE;} 
+	.ticheng li:nth-of-type(even){background-color: #fff;} 
+	.tc{
+		width: 80%;
+		margin: 10px auto;
+		height: 90%;
+		display: flex;justify-content: space-between;padding: 10px;line-height: 25px;font-size: 14px;}
+	.tc_l{color: #9A9A9A;}
+	.tc_l span{font-size: 14px;font-weight: bold;color: #000;}
+	.tc_2{color: #E1461D;}
+	.tc_2 span{color: #9A9A9A;}
+	.smallText{
+		color: #626262 !important;
+		font-size: 8rpx !important;
+	}
+}
+</style>
---
+++ pages/user/reseller/reseller.vue
@@ -0,0 +1,364 @@
+<template>
+	<view class="reseller">
+		<view class="top">
+			 <view class="uni-flex uni-column">
+				 <view  style="width: 100%;height:35%;">
+					 <view class="title-head">
+					 	<navigator url="/pages/user/record/record">
+					 		<view class="jl">预演环境业务测试</view>
+							<view style="font-size: 15px; margin-top: 20px;">今日收入(元)</view>
+					 	</navigator>
+					 </view>
+				 </view> 
+				<view class="BH-TOP"></view>
+				<view style="display: block;width:90%;margin:0 auto; height:50%; background: #FFFFFF;border: 1px solid #FFFFFF; border-radius: 5px;margin-bottom:30px;">
+						<view class="middle-top">
+							<view class="flex">
+								<view class="flex-sub  padding-sm margin-xs top-text">0.00</view>
+								<view class="flex-sub  padding-sm margin-xs top-right">
+									<navigator url="/pages/user/record/record">
+										<view class="jl">详情>></view>
+									</navigator>
+								</view>
+							</view>
+						</view>
+						<view class="BH">
+							--------------------------------------------------------------------
+						</view>
+						<view class="middle-bottom" style="margin-right: 75px;">
+							<view class="flex">
+								<view class="flex-sub  padding-sm margin-xs ">
+									<view>昨日收益</view>
+									<view class="bottom-text">0</view>
+								</view>
+								<view class="flex-sub  padding-sm margin-xs">
+									<view>累计已提</view>
+									<view class="bottom-text">0</view>
+								</view>
+							</view>
+						</view>
+					
+					</view>
+					<view  style="width: 100%;height:15%;display: block;background:#FFFFFF;">
+						<view class="flex">
+							<view class=" padding-top  margin-xs ">
+								 <img src="@/imgs/6.jpg" style="margin-left: 30%;">
+							</view>
+							<view class="flex-sub  padding-middle  margin-xs ">
+								<swiper class="swiper" vertical="true" autoplay="false" duration="500" interval="5000">
+										 <swiper-item v-for="(item, index) in msg" :key="index">
+											  <navigator :url="'/pages/user/notice/text?item='+ encodeURIComponent(JSON.stringify(item))">
+											     <view>第{{item}}个会员充值成功</view>
+											  </navigator>
+										 </swiper-item>
+								 </swiper>
+							</view>
+							<view class="padding-top margin-xs">
+								 <img src="@/imgs/6.jpg" style="float: right;margin-right: 30%;">
+							</view>
+						</view>
+					</view>
+				</view>
+		</view>
+		<view class="icon">
+			<view class="ico" v-for="item of list">
+				<navigator :url="item.url">
+					<view class="tubiao">
+						<img class="img" :src="item.img"></img>
+					</view>
+					<view class="tt">
+						<view class="text">{{item.title}}</view>
+					</view>
+				</navigator>
+			</view>
+		</view>
+		<view class="kong"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg:[
+					"1",
+					"2"
+				],
+				list: [{
+						title: '推广名片',
+						img: require("@/imgs/mp.png"),
+						url: "/pages/user/spread/spread"
+					},
+					{
+						title: '推广人统计',
+						img: require("@/imgs/tj.png"),
+						url: "/pages/user/agent/agent"
+					},
+					{
+						title: '佣金明细',
+						img: require("@/imgs/q.png"),
+						url: "/pages/user/mingxi/mingxi"
+					},
+					{
+						title: '推广排名',
+						img: require("@/imgs/dd.png"),
+						url: "/pages/user/sort/sort"
+					},
+				]
+			}
+		},
+		components: {},
+		methods: {
+			jump_cash() {
+				uni.navigateTo({
+					url: '/pages/user/cash/cash',
+				});
+			}
+		},
+		onPullDownRefresh() {
+			setTimeout(function() {
+				uni.stopPullDownRefresh();
+			}, 2000);
+		}
+	}
+</script>
+
+<style lang="less">
+	page {
+		background-color: #F5F5F5;
+	}
+
+	.reseller {
+		background-color: #F5F5F5;
+		min-height: 100vh;
+		font-size: 14px;
+
+		.top {
+			color: #FFFFFF;
+			display: flex;
+			justify-content: space-between;
+			position: relative;
+			width: 100%;
+			height: 350px;
+			background: #29b105; 
+			//linear-gradient(top, #4EC28E, #1C9C63);
+			.title {
+				background-color: #FFFFFF
+			}
+			.uni-flex {
+				width: 100%;
+				display: flex;
+				flex-direction: row;
+			}
+			
+			.uni-column {
+				flex-direction: column;
+			}
+			
+			.flex-item {
+				width: 100%;
+				height: 200rpx;
+				text-align: center;
+				line-height: 200rpx;
+			}
+			
+			.flex-item-V {
+				width: 100%;
+				height: 150rpx;
+				text-align: center;
+				line-height: 150rpx;
+			}
+			
+			.middle-top{
+				width: 100%;
+				height: 40%;
+				margin-top: 2%;
+			
+				.flex {
+					/* #ifndef APP-NVUE */
+					display: flex;
+					/* #endif */
+					flex-direction: row;
+					margin-left: 10%;
+				}
+				
+				.flex-bottom {
+					/* #ifndef APP-NVUE */
+					display: flex;
+					/* #endif */
+					flex-direction: row;
+					margin-left: 10%;
+				}
+				
+				
+				
+				.flex-sub {
+					width: calc(100% - 96upx - 60upx - 20upx);
+				}
+				
+				.top-text{
+					font-size: 40px;
+					color: #000000;
+					font-weight: 500;
+				}
+				
+				.top-right{
+					margin: auto 0;
+					color: #FAAC17;
+					margin: auto 0 !important;
+				}
+				
+				.bg-grey {
+					background-color: #8799a3;
+					color: #ffffff;
+				}
+				
+				.padding-sm {
+					padding: 20upx;
+				}
+				
+				.margin-xs {
+					margin: 10upx;
+				}
+			}
+			
+			.middle-bottom{
+				width: 100%;
+				height: 55%;
+				color: #9B9B9B;
+				margin-left: 10%;
+				.bottom-text{
+					font-size: 30px;
+					margin-top: 10px;
+					margin-left: 10px
+				}
+			}
+
+			.num {
+				font-size: 15px;
+				line-height: 25px;
+				text-align: center;
+				margin-top: 33%;
+				width: 33%;
+
+			}
+		}
+
+		.jilu {
+			font-size: 15px;
+			position: absolute;
+			left: 65%;
+			margin-top: 23%;
+		}
+
+		.title-head {
+			font-size: 20px;
+			position: absolute;
+			left: 5%;
+			top: 7%;
+		}
+
+		.tixian {
+			display: flex;
+			justify-content: center;
+			background-color: #F5F5F5;
+			width: 55%;
+			height: 60px;
+			border-radius: 60px;
+			position: absolute;
+			bottom: -30px;
+			left: 23%;
+
+			.btn {
+				margin-top: 7%;
+				text-align: center;
+				width: 85%;
+				background-color: #1C9C63;
+				height: 40px;
+				border: none;
+				border-radius: 50px;
+				line-height: 40px;
+				color: #FFFFFF;
+			}
+		}
+
+		.icon {
+			display: flex;
+			justify-content: space-between;
+			flex-wrap: wrap;
+
+			margin-left: 3%;
+			margin-right: 3%;
+			margin-top: 5%;
+
+			.ico {
+				border-radius: 10px;
+				border: 1px solid #C0C0C0;
+				width: 49%;
+				height: 100px;
+				margin-top: 10px;
+				display: flex;
+				border: none;
+				font-size: 15px;
+				flex-direction: column;
+				background-color: #FFFFFF;
+
+				.img {
+					width: 32px;
+					height: 32px;
+				}
+
+				.tubiao {
+					margin-top: 20px;
+					text-align: center;
+				}
+
+				.text {
+					color: #8F8F94;
+					padding-top: 5px;
+					text-align: center;
+
+				}
+			}
+		}
+		
+		.swiper{
+		   margin-top: 3%;
+		   font-size: 13px;
+		   text-align: center;
+		   padding: auto 0;
+		}
+		
+		
+		.kong {
+			height: 100px;
+		}
+
+		.BH {
+			background-color: #9B9B9B;;
+			height: 1px;
+		}
+		
+		.BH-TOP {
+			background-color: #9B9B9B;;
+			height: 5px;
+			margin-left: 2%;
+			margin-right: 2%;
+		}
+
+		.head-title {
+			font-size: 20px;
+			width: 300px;
+			margin-top: 10px
+		}
+		
+		.padding-top{
+			padding: 5rpx;
+		}
+		
+		.padding-middle{
+			padding: 10rpx;
+			color: #9B9B9B;
+		}
+	}
+</style>
